63f9d16868a1cc550c799d6c48e2324eacbf7be1
=Cesar Puma <=puma.rasec@yahoo.es>
2018-08-20 01:23:04 -0500

    Ejercicios  hasta clase 7 completas

--- lesson_06/EXERCISE6.py
+++ lesson_06/EXERCISE6.py
@@ -1,53 +1,63 @@
+import re
 class CipherBase:
 
-    def __init__(self, encrypt_message):
+    def __init__(self, encrypt_message, frecuency ):
         self.encrypt_message =  encrypt_message
         self.decrypt_message = ""
+        self.frecuency= frecuency
         pass
 
     def __decrypt(self):
-        L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", range(26)))
-        I2L = dict(zip(range(26), "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+        count = {}
+        for i in self.encrypt_message.lower():
+            if re.match((r'[a-z]'),i)!= None: # Use re.math solo por jugar con .isalpha() salia igual
+                if  i in count.keys():
+                    count[i] = count[i] + 1
+                else:
+                    count[i] = 1
+        l = list(count.items())
+        l.sort(key=lambda x:x[1], reverse=True)
 
-        print(L2I)
-        print(I2L)
-        key = 3
-        for c in self.encrypt_message.upper():
-            if c.isalpha():
-                self.decrypt_message += I2L[(L2I[c] - key) % 26]
+        frec = list(zip( self.frecuency.lower(), range(27)))
+
+        comparacion = dict((l[i][0], frec[i][0]) for i in range(0, len(l)) )
+
+        self.decrypt_message = ""
+        for i in self.encrypt_message:
+            if i.isalpha():
+                if i.isupper() :
+                    self.decrypt_message  += str(comparacion[i.lower()]).upper()
+                elif i.islower():
+                    self.decrypt_message  += str(comparacion[i])
             else:
-                self.decrypt_message += c
-        pass
+                self.decrypt_message  += i 
+
+        return self.decrypt_message 
+
+
+
+
 
     def print_decrypt_message(self):
         self.__decrypt()
-        return  (str(self.decrypt_message))
+        print(self.decrypt_message)
+        
 
     def __str__(self):
         pass
 
-cifrado = "TEOAISRHNUCMDLGWFPYKJBVQX"
-descifrado = "De mas frecuente a menos frecuente"
 
+mensaje_cifrado = '''
+ Bgc-bfufb tegaedppqna ql aggv zge xof tegaedppfe'l lgjb.
+    Xof adpf vflqanfe logjbvn'x hf pdwqna d cgebv qn coqro xof tbdkfe ql mjlx d lpdbb tdex. Xof tbdkfe QL XOF HGLL; qx'l kgje vjxk xg fnxfexdqn oqp ge ofe.
+    Zgrjl ql d pdxxfe gz vfrqvqna codx xoqnal kgj def ngx agqna xg vg.
+    Xof rglx gz dvvqna d zfdxjef qln'x mjlx xof xqpf qx xdwfl xg rgvf qx. Xof rglx dblg qnrbjvfl xof dvvqxqgn gz dn ghlxdrbf xg zjxjef fstdnlqgn. Xof xeqrw ql xg tqrw xof zfdxjefl xodx vgn'x zqaox fdro gxofe. - Mgon Rdepdrw.
 
+    (ccc.adpdljxed.rgp/uqfc/nfcl/234346?utkjpvbjr)
 
-example_cipher = CipherBase(cifrado)
+    (ccc.hedqnkijgxf.rgp/ijgxfl/djxogel/m/mgon_rdepdrw.oxpb)
+    '''
+frecuencia = "TEOAISRHNUCMDLGWFPYKJBVQX"
+example_cipher = CipherBase(mensaje_cifrado, frecuencia)
+example_cipher.print_decrypt_message()
 
-L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", range(26)))
-I2L = dict(zip(range(26), "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-
-print(cifrado)
-print(example_cipher.print_decrypt_message())
-
-
-print( len(cifrado))
-print( len(descifrado))
-
-cadena = []
-for i in cifrado:
-    for k, value in L2I.items():
-           if i == k:
-               cadena.append('{}: {}'.format(k, value))
-
-print(cadena)
-print(L2I.items())
